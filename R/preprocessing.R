cb30140d2e85dcf53fcdc56e423c9f70f8860f96
seungwoo <seungwoolee99@gmail.com>
2023-02-09 18:05:44 +0900

    FEAT : data preprocessing - 20개월 이하 데이터 처리(total_20 변수 생성), 각 월령별 도축률 계산(rate 변수 생성)

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -2,39 +2,77 @@ library(readxl)
 library(tidyr)
 library(tidyverse)
 library(reshape2)
+getwd()
 setwd("Desktop/Predict-slaughter/")
+rm(list=ls())
 
-breeding <- read_xlsx("Dataset/14_22사육.xlsx", sheet = 2) %>% 
+breeding <- read_xlsx("Dataset/14_22사육.xlsx", sheet = 2) %>%
   separate(구분...1, sep = "년", into = c("year", "month")) %>% 
-  mutate(
-    month = parse_number(month)
-  ) %>% 
   filter(구분...3 != '소계') %>%
-  pivot_longer(cols= 5:43, values_to = "breeding_count", names_to = "age")  %>% 
+  pivot_longer(cols= 5:43, values_to = "breeding_count", names_to = "age")  %>%
   rename("kind" = "구분...2", "gender" = "구분...3") %>% 
-  mutate(type = "breeding")
+  mutate(
+    month = parse_number(month),
+    type = "breeding",
+    age = parse_number(age)
+    )
 
 slaughter <- read_xlsx("Dataset/14_22도축.xlsx", sheet = 2) %>% 
   separate(구분...1, sep = "년", into = c("year", "month")) %>% 
+  filter(구분...3 != '소계') %>% 
+  pivot_longer(cols= 5:26, values_to = "slaughter_count", names_to = "age") %>%
   mutate(
-    month = parse_number(month)
+    month = parse_number(month),
+    age = parse_number(age),
+    type = "slaughter",
+    age = ifelse(age == 20, "total_20", age)
   ) %>% 
-  filter(구분...3 != '소계') %>%
-  pivot_longer(cols= 5:26, values_to = "slaughter_count", names_to = "age") %>% 
-  rename("kind" = "구분...2", "gender" = "구분...3")  %>% 
-  mutate(type = "slaughter")
+  rename("kind" = "구분...2", "gender" = "구분...3")
+  
 
-df <- left_join(breeding, slaughter, by=c('year'='year', 'month'='month', 'kind' = 'kind', 'gender'='gender', 'age'='age')) %>% 
+breeding_under20 <- breeding %>% 
   mutate(
-    rate = (slaughter_count/(breeding_count+slaughter_count))*100
+    age = ifelse(age <=20, "total_20", age)
+    ) %>%
+  group_by(year, month, kind, gender, age, type) %>% 
+  summarise(breeding_count = sum(breeding_count, na.rm=TRUE)) %>% 
+  filter(age == "total_20")
+View(breeding)
+breeding <- merge(breeding, breeding_under20, all=TRUE)
+
+total <- merge(breeding, slaughter, by=c('year'='year', 'month'='month', 'kind' = 'kind', 'gender'='gender', 'age'='age'), all=TRUE) %>% 
+  mutate(
+    rate = (slaughter_count/(breeding_count+slaughter_count))
   ) %>% 
   select(-c(type.x,type.y))
-View(df)
-total <- rbind(breeding, slaughter) 
-View(total)
 
-hanwoo_cow <- total %>% 
-  filter(kind == "한우") %>% 
-  filter(gender == "암")
+View(total)
+test <- total %>% 
+  filter(year == 2022) %>% 
+  View()
+
+train <- total %>% 
+  filter(year != 2022) 
+
+rate <- train %>% 
+  group_by(kind, gender, age) %>% 
+  summarise(mean_rate = mean(rate, na.rm=TRUE))
+View(rate)
+total %>% 
+  filter(year == 2021 & month == 12) %>% 
+  mutate(
+    year = 2022,
+    month = 1
+  ) %>%  
+  subset(select = c(year, month, gender, age, kind,breeding_count)) %>% 
+  filter(kind=="한우" & gender=="암") ->predictJan 
+  
+predictJan$predict_breeding_count <- lag(predictJan$breeding_count)
+predictJan
+
+
+
+
+
 
 
